22cf635a94c06dbd90b54810f7b2c971cfcbc295
daniel48kassem <k1999daniel@gmail.com>
2021-04-05 15:57:45 -0400

    application UI improvements

--- components/jobs/CompanySingleJob.vue
+++ components/jobs/CompanySingleJob.vue
@@ -6,44 +6,47 @@
           <v-list-item :to="'my-jobs/'+jobad.data.id">
             <v-list-item-title class="blue--text headline">
               {{jobad.data.attributes.title}}
-              <v-btn x-small outlined color="red" v-if="jobad.data.attributes.approved_at">Approved</v-btn>
-              <v-btn color="blue" outlined v-if="!jobad.data.attributes.approved_at">Waiting For Approval</v-btn>
             </v-list-item-title>
           </v-list-item>
         </v-list>
+        <v-btn class="apporval-btn" x-small color="red" outlined v-if="!jobad.data.attributes.approved_at">Waiting For
+          Approval
+        </v-btn>
+        <v-btn class="apporval-btn" x-small color="teal" outlined v-if="jobad.data.attributes.approved_at">Approved
+        </v-btn>
       </v-card-title>
       <v-divider>s</v-divider>
 
       <v-card-text class="subtitle">
         <v-list>
           <v-list-item>
-              <p>
-                <v-icon>mdi-map-marker</v-icon>
-                {{jobad.data.attributes.location}}
-              </p>
-              <p class="ml-2">
-                <v-icon>mdi-calendar-month</v-icon>
-                {{jobad.data.attributes.approved_at}}
-              </p>
-              <p class="ml-2">
-                <v-icon>mdi-clock-time-four-outline</v-icon>
-                {{ jobad.data.attributes.job_time}}
-              </p>
-              <p class="ml-2">
-                <v-icon>
-                  mdi-briefcase
-                </v-icon>
-                {{jobad.data.attributes.job_type}}
-              </p>
-              <p class="ml-2">
-                <v-icon>
-                  mdi-cash-multiple
-                </v-icon>
-                from
-                ${{jobad.data.attributes.min_salary}}
-                to
-                ${{jobad.data.attributes.max_salary}}
-              </p>
+            <p>
+              <v-icon>mdi-map-marker</v-icon>
+              {{jobad.data.attributes.location}}
+            </p>
+            <p class="ml-2">
+              <v-icon>mdi-calendar-month</v-icon>
+              {{jobad.data.attributes.approved_at}}
+            </p>
+            <p class="ml-2">
+              <v-icon>mdi-clock-time-four-outline</v-icon>
+              {{ jobad.data.attributes.job_time}}
+            </p>
+            <p class="ml-2">
+              <v-icon>
+                mdi-briefcase
+              </v-icon>
+              {{jobad.data.attributes.job_type}}
+            </p>
+            <p class="ml-2">
+              <v-icon>
+                mdi-cash-multiple
+              </v-icon>
+              from
+              ${{jobad.data.attributes.min_salary}}
+              to
+              ${{jobad.data.attributes.max_salary}}
+            </p>
           </v-list-item>
 
           <v-list-item class="text--black text-md-body-1">
@@ -82,4 +85,7 @@
 </script>
 
 <style scoped>
+  .apporval-btn {
+    transform: rotate(45deg);
+  }
 </style>
--- components/jobs/SingleApplication.vue
+++ components/jobs/SingleApplication.vue
@@ -5,35 +5,101 @@
     max-width="400px"
     max-height="400px"
   >
-    <v-card-title>
-      <v-icon
-        large
-        left
+    <v-row justify="end">
+      <v-menu
+        transition="slide-y-transition"
+        bottom
       >
-        mdi-twitter
-      </v-icon>
-      <span class="title font-weight-bold">
-            {{application.data.attributes.user.data.attributes.name}}
-          </span>
-    </v-card-title>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            color="primary"
+            dark
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-account-cog-outline </v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="(item, i) in items" :key="i">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-row>
 
-    <v-card-text class="headline font-weight-bold">
-      <v-text-field
-        disabled
-        :value="application.data.attributes.user.data.attributes.email"
-        outlined>
-      </v-text-field>
+    <v-row>
+        <v-col md="4">
+          <v-avatar size="120">
+            <img
+              alt="user"
+              src="https://philadelphiapsychology.org/wp-content/uploads/2018/07/male-profile-picture.png"
+            >
+          </v-avatar>
+        </v-col>
+        <v-col md="8">
+          <v-card-title class="headline">
+            <v-list-item to="/user/profile">
+              {{application.data.attributes.user.data.attributes.name}}
+            </v-list-item>
+          </v-card-title>
 
-      Uploaded Cv
-      <v-btn text  exact-active-class="no-active">
-        {{application.data.attributes.cv.data.attributes.title}}
-        <a target="_blank"
-          :href="'backend'+application.data.attributes.cv.data.attributes.download_link">
-          <v-icon>mdi-download</v-icon>
-        </a>
-      </v-btn>
+          <v-card-text class="text-body-1">
+          <p><v-icon>mdi-cake-variant </v-icon>12,april,1983</p>
+          <p> <v-icon>mdi-email</v-icon>{{application.data.attributes.user.data.attributes.email}}</p>
+             <p><v-icon>mdi-phone</v-icon>0931869085</p>
+          <p><v-icon>mdi-map-marker</v-icon>Latakia</p>
+            <strong>
+              <v-icon>mdi-file-account</v-icon>Cv</strong>
+            <v-btn x-small outlined color="blue-grey">
+              {{application.data.attributes.cv.data.attributes.title}}
+              <a target="_blank"
+                 :href="'backend'+application.data.attributes.cv.data.attributes.download_link">
+                <v-icon>mdi-download</v-icon>
+              </a>
+            </v-btn>
+          </v-card-text>
+          <v-card-actions>
+          </v-card-actions>
+        </v-col>
+      </v-row>
 
-    </v-card-text>
+
+
+<!--    <v-card-title>-->
+<!--      <v-list-item to="/profile/user">-->
+<!--        <v-icon-->
+<!--          large-->
+<!--          left-->
+<!--        >-->
+<!--          mdi-twitter-->
+<!--        </v-icon>-->
+<!--        <span class="title font-weight-bold">-->
+<!--            {{application.data.attributes.user.data.attributes.name}}-->
+<!--          </span>-->
+<!--      </v-list-item>-->
+<!--    </v-card-title>-->
+<!--    <v-card-text class="font-weight-bold">-->
+
+<!--      user email-->
+<!--      <v-list-item>-->
+<!--        {{application.data.attributes.user.data.email}}-->
+<!--      </v-list-item>-->
+
+<!--      <v-row>-->
+<!--        Uploaded Cv-->
+<!--        <v-spacer/>-->
+<!--        <v-btn text exact-active-class="no-active">-->
+<!--          {{application.data.attributes.cv.data.attributes.title}}-->
+<!--          <a target="_blank"-->
+<!--             :href="'backend'+application.data.attributes.cv.data.attributes.download_link">-->
+<!--            <v-icon>mdi-download</v-icon>-->
+<!--          </a>-->
+<!--        </v-btn>-->
+<!--      </v-row>-->
+
+
+<!--    </v-card-text>-->
   </v-card>
 </template>
 
@@ -44,6 +110,16 @@
       application: {
         type: Object,
         required: true
+      },
+      data() {
+        return {
+          items: [
+            { title: 'Click Me' },
+            { title: 'Click Me' },
+            { title: 'Click Me' },
+            { title: 'Click Me 2' },
+          ],
+        }
       },
     },
   }
